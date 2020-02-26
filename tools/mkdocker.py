3e5e2f88ababba0e3f3d8043f6823fed096f53b6
Mingyi Zhang <mingyi.z@outlook.com>
2020-02-26 18:48:33 +0800

    .

--- tools/mkdocker.py
+++ tools/mkdocker.py
@@ -14,6 +14,9 @@ import jinja2
 import argparse
 from urllib.parse import urlparse
 
+_DIR = os.path.join(os.path.dirname(__file__), '..')
+_DIR = os.path.normpath(os.path.abspath(_DIR))
+
 class Config(object):
     mirror = None
     http_proxy  = None    
@@ -37,13 +40,11 @@ class Config(object):
 
 class Dockerfile(object):
 
-    def __init__(self, name, version, dir=None, config=None):
+    def __init__(self, name, version, config=None):
       self._name = name
-      self._dir = os.path.abspath(dir or 'tools/docker')
-      print('-------DIR-', self._dir)
-      self._version = version #or m.__version__
+      self._dir = os.path.join(_DIR, 'tools', 'docker')
+      self._version = version
       self._config = config or Config()
-
 
     def _render(self, **kwargs):
         if not os.path.exists(os.path.join(self._dir, self._name +'.j2')):
@@ -79,31 +80,20 @@ def main():
     parser.add_argument('--version', help="configure file path.")
     parser.add_argument('-c', '--config', help="configure file path.")
     parser.add_argument('-B', '--build', default=False, action="store_true", help="build.")
-    prj_dir = os.path.join(os.path.dirname(__file__), '..')
-    print('1',prj_dir)
-    prj_dir = os.path.normpath(os.path.abspath(prj_dir))
-    print('2', prj_dir)
-    sys.path.insert(0, prj_dir)
+    sys.path.insert(0, _DIR)
     import epm
     print('EPM', epm)
-
-#    parser.add_argument('--root-dir')
     args = parser.parse_args()
     config = Config(args.config or None)
     name = args.name[0]
-#    if args.root_dir:
-    os.chdir(prj_dir)
-#    print('WORKING AT:', os.path.abspath('.'))
-
-#    import epm
     filename = 'Dockerfile-%s' % name
     version = args.version or epm.__version__
-    docerfile = Dockerfile(name, version, dir=prj_dir, config=config)
-    docerfile.write(filename)
+    docerfile = Dockerfile(name, version, config=config)
+    docerfile.write(os.path.join(_DIR, filename))
     if args.build:
         import subprocess
         command = ['docker', 'build', '.', '-f', filename, '-t', 'epmkit/%s:%s' % (name, version)]
-        subprocess.run(command, check=True)
+        subprocess.run(command, check=True, cwd=_DIR)
 
 
 if __name__ == '__main__':
