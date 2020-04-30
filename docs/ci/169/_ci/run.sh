8a871e5b8bc352d94c88f4ad52bf7c28996c1efe
Mingyi Zhang <mingyi.z@outlook.com>
2020-04-30 18:54:35 +0800

    .

---
+++ docs/ci/169/_ci/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${currentDir}/base/${1}.sh
+_localhost=${2}
+_script=${3}
+_step=${4}
+echo $_script  $_step
+if [ ${_step} == 'build_script' ]; then
+script=$(cat ${_script})
+
+sshpass -p ${_CI_SSH_PASSWORD} ssh -p ${_CI_SSH_PORT} admin@${_CI_HOSTNAME} -o StrictHostKeyChecking=no << EOF
+umount -f ${_CI_BUILDS_DIR} >/dev/null 2>&1
+[ ! -d ${_CI_BUILDS_DIR} ] && mkdir -p ${_CI_BUILDS_DIR}
+mount -t nfs -o nolock ${_localhost}:${_CI_BUILDS_DIR} ${_CI_BUILDS_DIR}
+export EPM_SANDBOX_RUNNER=shell
+export EPM_SANDBOX_HOME=/tmp
+/bin/sh -c ${script}
+EOF
+
+
+#sshpass -p ${_CI_SSH_PASSWORD} ssh -p ${_CI_SSH_PORT} admin@${_CI_HOSTNAME} -o StrictHostKeyChecking=no << EOF
+#export EPM_SANDBOX_RUNNER=shell
+#/bin/sh -c ${script}
+#EOF
+else
+  export PATH=/home/epm:$PATH
+  /bin/bash ${_script}
+fi
+
