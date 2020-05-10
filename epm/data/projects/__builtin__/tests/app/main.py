29020d8624ae7721b7ac19d598cfd4c3f530331a
Mingyi Zhang <mingyi.z@outlook.com>
2020-05-10 13:34:09 +0800

    .

--- epm/data/projects/__builtin__/tests/app/main.py
+++ epm/data/projects/__builtin__/tests/app/main.py
@@ -5,11 +5,11 @@ import unittest
 if __name__ == '__main__':
     
     pattern = "test*.py"
-    if len(sys.argv) >2:
-      pattern = sys.argv[2]
+    if len(sys.argv) > 2:
+        pattern = sys.argv[2]
       
     cases = unittest.defaultTestLoader.discover(
-    os.path.dirname(__file__), pattern=pattern,top_level_dir=None)
+    os.path.dirname(__file__), pattern=pattern, top_level_dir=None)
 
     runner = unittest.TextTestRunner()
     runner.run(cases)
