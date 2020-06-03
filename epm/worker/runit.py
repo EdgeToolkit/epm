0c0e92a6a87ee0a54319491fe3432a6a0567b99c
Mingyi Zhang <mingyi.z@outlook.com>
2020-06-03 17:32:17 +0800

    @204

--- epm/worker/runit.py
+++ epm/worker/runit.py
@@ -33,8 +33,9 @@ class Runner(object):
 
     def exec(self, command, argv):
         runner = ConanRunner(output=self._api.out)
-        print(command + argv, runner, '\n----', __file__, 'out', self._api.out)
+
         command = ['echo', 'ABCDEFG HIJKLMN!']
+        print(command + argv, runner, '\n  ----', __file__, 'out', self._api.out)
 
         return runner(command + argv)
 
