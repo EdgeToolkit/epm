66c3ef30206864e5ff0d5fb34f51747205947f51
Mingyi Zhang <mingyi.z@outlook.com>
2020-06-03 17:28:15 +0800

    @204

--- epm/model/runner.py
+++ epm/model/runner.py
@@ -13,6 +13,9 @@ class Output(ConanOutput):
     def write(self, data, front=None, back=None, newline=False, error=False):
         #if PLATFORM == 'Windows':
         print('*', len(data))
+        n = len(data)
+        for i in range(0, n):
+            print(i, data[i], '*')
         data = data.replace('\r\n', '\n')
         super(Output, self).write(data, front, back, newline, error)
 
