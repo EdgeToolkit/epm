99a220c20197032c33ffc05ac06083d81b2a33f0
Mingyi Zhang <mingyi.z@outlook.com>
2020-12-16 14:15:56 +0800

    17

--- docker-tools/build.sh
+++ docker-tools/build.sh
@@ -3,4 +3,4 @@ _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${_DIR}/..
 [ ! -d .epm ] && mkdir -p .epm
 git archive --format=tar.gz --output .epm/epm.tar.gz HEAD
-sudo -E ./docker-tools/main.py $*
+echo sudo -E ./docker-tools/main.py $*
