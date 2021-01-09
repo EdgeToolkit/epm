a35f4b0d347df84aa77d789f214fce46d17a6864
Mingyi Zhang <mingyi.z@outlook.com>
2021-01-09 23:41:43 +0800

    .

--- epm/worker/runx.py
+++ epm/worker/runx.py
@@ -1,5 +1,5 @@
 import os
-from epm import DATA_DIR
+from epm import EXTENSIONS_DIR as EX_DIR
 from epm.utils import abspath, load_module
 from epm.worker import Worker
 from epm.tools.extension import Definition
@@ -13,53 +13,47 @@ class RunX(Worker):
         self.name = name
         self.namespace = namespace
         self.project = project
-        self.api = api
-        self._builtin_prototype = None
-        self._builtin_extension = None
+        self._builtin = None
+        self._definition = None
         self.workbench = os.environ.get('EPM_WORKBENCH') or None
-        if not self.api and self.project:
-            self.api = self.project.api
 
-        self.definition = Definition.load(name,project, os.environ.get('EPM_WORKBENCH'))
 
     @property
-    def builtin_prototype(self):
-        if self._builtin_prototype is None:
-            p_dir = os.path.join(DATA_DIR, 'extension', 'prototype')
-            self._builtin_prototype = {}
-            for name in os.listdir(p_dir):
-                path = os.path.join(p_dir, name)
-                if os.path.isdir(path):
-                    self._builtin_prototype[name] = path
-        return self._builtin_prototype
+    def builtin(self):
+        if self._builtin is None:
+            self._builtin = {}
+            for name in os.listdir(EX_DIR):
+                path = os.path.join(EX_DIR, name)
+                if os.path.exists(f'{path}/extension.yml'):
+                    self._builtin[name] = path
+        return self._builtin
 
     @property
-    def builtin_extension(self):
-        if self._builtin_extension is None:
-            p_dir = os.path.join(DATA_DIR, 'extension')
-            self._builtin_extension = {}
-            for name in os.listdir(p_dir):
-                path = os.path.join(p_dir, name)
-                if name in ['prototype']:
-                    continue
-                if os.path.isdir(path):
-                    self._builtin_prototype[name] = path
-        return self._builtin_extension
-
-
-    def _load_extension(self):
-        try:
-            definition = Definition.load(self.name, self.namespace, self.project, self.workbench)
-        except FileNotFoundError as e:
-            if self.namespace in [None, '', 'epm'] and self.name in self.builtin_extension:
-                ext_dir = abs(f'~/.epm/extension/epm')
-                if self.workbench:
-                    ext_dir =  abspath(f'~/.epm/.workbench/{self.workbench}/extension/epm')
-                shutil.copytree(self.builtin_extension[self.name], ext_dir)
+    def definition(self):
+        if self._definition is None:
+            try:
                 definition = Definition.load(self.name, self.namespace, self.project, self.workbench)
-            else:
-                raise e
-        attribute = definition.attribute
+            except FileNotFoundError as e:
+                if self.namespace in [None, '', 'epm'] and self.name in self.builtin:
+                    ex_dir = abspath(f'~/.epm/extension/epm')
+                    if self.workbench:
+                        ex_dir =  abspath(f'~/.epm/.workbench/{self.workbench}/extension/epm')
+                    shutil.copytree(self.builtin[self.name], ex_dir)
+                    definition = Definition.load(self.name, self.namespace, self.project, self.workbench)
+                else:
+                    raise e
+            self._definition = definition
+        return self._definition
+
+    def main(self):
+        if self.definition.kind == 'prototype':
+            raise Exception('prototype of extension is not runnable.')
+
+        if self.definition.prototype:
+            # load prototype
+
+
+
 
 
 
