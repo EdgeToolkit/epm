6e50040f075614d237bb4c4372ab3368a5c2ade7
Mingyi Zhang <mingyi.z@outlook.com>
2021-01-12 14:07:12 +0800

    0.40

--- epm/__init__.py
+++ epm/__init__.py
@@ -5,6 +5,7 @@ import os
 __version__ = '0.0.40'
 
 
+
 HOME_DIR = os.path.join(os.path.expanduser('~'), '.epm')
 DATA_DIR = os.path.normpath('%s/data' % os.path.dirname(__file__))
 EXTENSIONS_DIR = os.path.normpath('%s/extensions' % os.path.dirname(__file__))
