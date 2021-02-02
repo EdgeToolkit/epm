5321ec63646b260e6d7aa3af649800b8e0bcb1d3
Mingyi Zhang <mingyi.z@outlook.com>
2021-02-02 19:34:30 +0800

    .

--- epm/__init__.py
+++ epm/__init__.py
@@ -2,7 +2,7 @@
 # to allow refactors
 import os
 
-__version__ = '0.0.60.0'
+__version__ = '0.0.60.1'
 
 HOME_DIR = os.path.join(os.path.expanduser('~'), '.epm')
 DATA_DIR = os.path.normpath('%s/data' % os.path.dirname(__file__))
